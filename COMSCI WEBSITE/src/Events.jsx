99004f08b787080c28eec001182e1c75eb5dc815
John Gutierrez <75353216+canilo1@users.noreply.github.com>
2024-06-29 11:47:22 -0700

    First Pull Request
    213123

---
+++ COMSCI WEBSITE/src/Events.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { useState,useEffect,useRef } from "react";
+function Events(){
+<>
+<button id = "Events"className = "MenuButtons">Events</button>
+</>
+}
+export default Events;
