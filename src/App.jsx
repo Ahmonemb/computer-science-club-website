c227166c99c4109f94b0af54606002fde1fba066
John Gutierrez <75353216+canilo1@users.noreply.github.com>
2024-07-09 19:33:20 -0700

    Incorrect code[Updated one]
    1

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import Officer from './Officer'
+
+import Home from './Home';
+
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+    <Home />
+    <Officer />
+    </>
+  )
+}
+
+export default App
